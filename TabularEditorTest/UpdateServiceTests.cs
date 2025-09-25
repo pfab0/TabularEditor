ccad3ec812a24198dec89a88503600881f587313
pfab0 <45492246+pfab0@users.noreply.github.com>
2025-09-25 19:48:00 +0200

    .

--- TabularEditorTest/UpdateServiceTests.cs
+++ TabularEditorTest/UpdateServiceTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TabularEditor.UIServices;
 
@@ -10,34 +10,34 @@ namespace TabularEditor
         [TestMethod]
         public void TestVersionCompareLogic()
         {
-            var current = Version.Parse("2.13.41451.2342");
+            var current = Version.Parse("2.27.1.2342");
 
             // Same version:
-            var check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.13.41451.2342"));
+            var check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.27.1.2342"));
             Assert.AreEqual(VersionCheckResult.NoNewVersion, check);
             Assert.IsFalse(check.UpdateAvailable());
             Assert.IsFalse(check.UpdateAvailable(true));
 
             // Lower version:
-            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.13.41450.1253"));
+            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.27.0.1253"));
             Assert.AreEqual(VersionCheckResult.NoNewVersion, check);
             Assert.IsFalse(check.UpdateAvailable());
             Assert.IsFalse(check.UpdateAvailable(true));
 
             // Patch available:
-            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.13.42643.1064"));
+            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.27.2.2342"));
             Assert.AreEqual(VersionCheckResult.PatchAvailable, check);
             Assert.IsTrue(check.UpdateAvailable());
             Assert.IsFalse(check.UpdateAvailable(true));
 
             // Minor available:
-            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.14.3123.144"));
+            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("2.28.0.144"));
             Assert.AreEqual(VersionCheckResult.MinorAvailable, check);
             Assert.IsTrue(check.UpdateAvailable());
             Assert.IsTrue(check.UpdateAvailable(true));
 
-            // Minor available:
-            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("3.0.10433.4344"));
+            // Major available:
+            check = VersionCheckResultExtension.DetermineUpdate(current, Version.Parse("3.0.0.4344"));
             Assert.AreEqual(VersionCheckResult.MajorAvailable, check);
             Assert.IsTrue(check.UpdateAvailable());
             Assert.IsTrue(check.UpdateAvailable(true));
