90642e419b6a26df7863ec881c3f2bc1a11da7b2
pfab0 <45492246+pfab0@users.noreply.github.com>
2025-09-30 21:25:28 +0200

    .

--- TabularEditor/UIServices/UpdateService.cs
+++ TabularEditor/UIServices/UpdateService.cs
@@ -1,12 +1,9 @@
+using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TabularEditor.UI;
-using Newtonsoft.Json.Linq;
 
 namespace TabularEditor.UIServices
 {
